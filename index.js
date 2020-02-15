0b584d181243af7e6757f3f0ab77165eb9acfe2b
Guilherme Argentino <guilherme.argentino@gmail.com>
2020-02-14 23:48:59 -0300

    Text Robot Implementation

---
+++ index.js
@@ -0,0 +1,29 @@
+const readline = require('readline-sync')
+const robots = {
+    text: require('./robots/text.js')
+}
+
+async function start() {
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
+    await robots.text(content)
+
+    function askAndReturnSearchTerm() {
+        return readline.question('Type a Wikipedia search term: ')
+    }
+
+    function askAndReturnPrefix() {
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+        return selectedPrefixText
+    }
+
+    console.log(content)
+}
+
+start()
